38b9a9a398924b02ab2b48305dacf0184e6e816b
Rifcode <rifkinurgraha69@gmail.com>
2024-10-01 13:32:09 +0700

    nambah konditional

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ bmi = berat_badan / (tinggi_badan ** 2)
 berat_badan_ideal = dict()
 berat_badan_ideal ['Min'] = 18.5 * (tinggi_badan**2)
 berat_badan_ideal ['Max'] = 25 * (tinggi_badan**2)
-print(f'nilai BMI anda adalah {bmi:.2f} kg/m^2')
 
-x = bmi >= 18.5
-y = bmi <= 25
 
-if x:
-    print("Berat anda sudah sesuai BMI")
-elif y:
-    print("Berat anda sudah sesuai BMI")
+if bmi< 18.5:
+    hasil = "Berat kamu masih kurang nihh"
+elif bmi < 25:
+    hasil = "Berat kamuu udah sesuai nihhh"
+elif bmi < 30:
+    hasil = " Kayaknyaa kamu harus sedikit diet deh"
 else:
-    print("Berat anda masih belum sesuai nih:((")
+    hasil = "Kamu terlalu berisi hahahaha"
 
-print(f'Berat Ideal BMI Anda adalah dalam rentang {berat_badan_ideal ['Min']} Kg - {berat_badan_ideal ['Max']} Kg')
+print(f'\nnilai BMI anda adalah {bmi:.2f} kg/m^2')
+print(hasil)
+print(f"Berat badan ideal anda adalah dalam rentang"
+      f"{berat_badan_ideal['Min']} Kg {berat_badan_ideal['Max']} Kg")
 print ('Terima Kasih Sudah Menggunakan Program Ini :))))')
